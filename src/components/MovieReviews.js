5326ed0c22a54aedb536d05a5dae9587d8de5b89
Liz P <parisielizabeth@gmail.com>
2021-03-13 00:47:34 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,6 +2,33 @@
 
 import React from 'react';
 
+const Review = ({
+    headline,
+    byline,
+    link,
+    summary_short
+}) => {
+    return (
+        <div 
+            key={headline}
+            className="review"
+        > 
+        <header> 
+            <a 
+            className="review-link"
+            href={link}
+            >
+                {headline}
+            </a>
+            <br></br>
+            <span className="author">{byline}</span>
+
+        </header>
+        <blockquote>{summary_short}</blockquote>
+        </div>
+    )  
+}
+
 const MovieReviews = ({reviews}) => (
   <div className='review-list'>
     {reviews.map(Review)}
